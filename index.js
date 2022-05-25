a820e362f71642349ed9891396e02bb3d4b97ac3
Toha92 <ahesan868@gmail.com>
2022-05-26 03:55:28 +0600

    user, order save to db implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const query = require("express/lib/middleware/query");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,17 +21,59 @@ async function run() {
   try {
     await client.connect();
     const toolsCollection = client.db("ComputerGarage").collection("tools");
+    const userCollection = client.db("ComputerGarage").collection("users");
+    const orderCollection = client.db("ComputerGarage").collection("orders");
 
     app.get("/tools", async (req, res) => {
       const tools = await toolsCollection.find().toArray();
       res.send(tools);
     });
+
+    app.get("/tools/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const tool = await toolsCollection.findOne(query);
+      res.send(tool);
+    });
+
+    app.put("/tools/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedTool = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: updatedTool,
+      };
+      const result = await toolsCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const user = req.body;
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send({ success: true, result });
+    });
   } finally {
   }
 }
 
-
-run().catch(console.dir)
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Computer is running");
